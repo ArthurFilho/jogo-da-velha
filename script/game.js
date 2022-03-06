8ab2f9820097bec7689e3dd5c72cd367aae2689e
Arthur <peraarthur2018@gmail.com>
2022-03-06 18:59:19 -0300

    segundo commit

--- script/game.js
+++ script/game.js
@@ -1,74 +1,76 @@
- //Hash Game
- 
- //start my variables
- 
- let board = ['','','','','','' ,'','','',''];
- 
- let playertime = 0;
-
+// Iniciar variáveis
+let board = ['', '', '', '', '', '', '', '', '',];
+let playerTime = 0;
+let symbols = ['o', 'x'];
 let gameOver = false
 
- let symbols = ['o', 'x'];
+let winStates = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+]
 
- let winStates = [
-  [0,1,2],
-  [3,4,5],
-  [6,7,8],
-  [0,3,6],
-  [1,4,7],
-  [2,5,8],
-  [0,4,8],
-  [2,4,6],
-  
-]  
 
- function handleMove(position){
-   if(gameOver){
-    return;
-   }
-      if(board[position] == '')
-      board[position] = symbols[playertime];
+// A função handleMove verifica quem fez o movimento e em qual quadrado ele fez o movimento, colocando assim um simbolo dentro do array de acordo com a vez do jogador.
 
-       
-      gameOver = isWin();
-      
-      
-      if(gameOver == false){
-      
-        if(playertime == 0)  {
-      
-          playertime = 1;
-       }
-      
-       else{
-      
-        playertime = 0;
-      }
-    }
-       return gameOver;
+function handleMove(position) {
 
+    if (gameOver) {
+        return;
 
     }
-     function isWin(){
 
-  
-       for(let i =0; i < winStates.length; i++){
-       let seq = winStates[i];
+    if (board[position] == '') {
+        board[position] = symbols[playerTime]
 
-      
-       let pos1 = seq[0];
-       let pos2 = seq[1];
-       let pos3 = seq[2];
+        gameOver = isWin();
 
-            if(board[pos1] == board[pos2] &&
-              board[pos1] == board[pos3]  &&
-              board[pos1] != ''){
+        if (gameOver == false) {
 
-                return true;
-            
-              }
-            }
-  
-  return false;
-  
-          }
+            playerTime = (playerTime == 0 ? 1 : 0)
+        }
+    }
+    return gameOver;
+}
+
+
+// Verificando se as combinações de jogadas são iguais as combinações vencedoras
+function isWin() {
+
+
+    // O for faz a verificação, se o a seq tiver os mesmos componentes que dão a vitória o jogo será finalizado
+    for (let i = 0; i < winStates.length; i++) {
+        let seq = winStates[i];
+
+        let pos1 = seq[0]
+        let pos2 = seq[1]
+        let pos3 = seq[2]
+
+        if (board[pos1] == board[pos2] && board[pos1] == board[pos3] && board[pos1] != '') {
+            return true
+        }
+    }
+    return false
+}
+
+function restart() {
+    board = ['', '', '', '', '', '', '', '', '',];
+    playerTime = 0;
+    symbols = ['o', 'x'];
+    gameOver = false
+
+    let squares = document.querySelectorAll(".square")
+
+    squares.forEach((square) => {
+        square.innerHTML = ""
+    })
+
+    let h1 = document.getElementsByTagName("h1")
+
+    h1[0].innerHTML = "<h1>Jogo da velha</h1>"
+}
--- script/interface.js
+++ script/interface.js
@@ -1,53 +1,53 @@
-document.addEventListener('DOMContentLoaded', ()=>{
+// Após carregar a página, a interface salvará todos os elementos da classe "square" dentro da variável squares
 
-let squares = document.querySelectorAll (".square");
+// Depois com o forEach iremos varrer o array e adicionar um evento para cada square quando for clicado
 
-squares.forEach((square)=>{
-square.addEventListener('click', handleClick);
+document.addEventListener('DOMContentLoaded', () => {
+
+  let squares = document.querySelectorAll(".square");
+
+  squares.forEach((square) => {
+      square.addEventListener('click', handleClick);
+  })
+
 })
 
+// A função handleClick mostra qual quadrado foi clicado, a variável square recebe esse quadrado e identifica ele pela sua ID
 
-})
+// A função handleMove é executada e o jogador da vez terá seu símbolo colocado na tela
 
-function handleClick(event){
-
+function handleClick(event) {
+  console.log(event.target);
 
   let square = event.target;
   let position = square.id;
 
-  if(handleMove(position)){
-
-    updateSquares();
-setTimeout(()=>{
-
-alert("O Jogo acabou!!! - O vencedor foi " + playertime + " e a sequencia foi " + 
-isWin );
-
-
-}, 10);
-
-
-
-
-
+  if (handleMove(position)) {
+    alert("O Jogador " + playerTime + 1 + " Venceu!!" )
   };
   updateSquares();
-
 }
 
-function updateSquares(){
+// O forEach irá varrer os quadrados, receberá qual a posição desse quadrado e irá receber o que tem dentro do array board, caso o mesmo esteja preenchido com algum símbolo, ele irá atualizar no HTML
 
-    let squares = document.querySelectorAll(".square");
 
-    squares.forEach((square)=>{
-        
-        let position = square.id;
-        let symbol = board[position];
-       
-       if(symbol != ''){
-           square.innerHTML = `<div class='${symbol}'></div>`;
+function updateSquares() {
+  let squares = document.querySelectorAll(".square");
 
-       }
+  squares.forEach((square) => {
+      let position = square.id;
+      let symbol = board[position]
 
-    })
+      if (symbol != '') {
+          square.innerHTML = '<div class=' + symbol + '></div>'
+      }
+  })
 }
+
+function btnClick() {
+  btn = document.querySelector(".btn")
+
+  btn.addEventListener("click", restart)
+}
+
+btnClick()
